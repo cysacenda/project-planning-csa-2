293b30ed0ab37b45087baf23191e3a66f1b3d616
csacenda <cysacenda@gmail.com>
2017-05-14 00:07:21 +0200

    Material design up and running !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MdDialog, MdDialogRef, MdSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
 import {HeaderComponent} from './shared/layout/header.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
+
+import {MdToolbarModule, MdMenuModule, MdButtonModule, MdIconModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,11 @@ import 'hammerjs';
     BrowserModule,
     FormsModule,
     HttpModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    MdMenuModule,
+    MdButtonModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [
