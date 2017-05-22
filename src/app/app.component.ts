0b25021f4baf7d3dbcd927c96a3bb8b06ee71bf4
csacenda <cysacenda@gmail.com>
2017-05-22 23:17:31 +0200

    Implémentation router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-/* Use mock data with service https://angular.io/docs/ts/latest/guide/style-guide.html
- It is a better practice to redistribute the component and its supporting classes into their own, dedicated files.*/
 export class AppComponent {
 
 }
