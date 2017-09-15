65d8dfcd8fe5a897be3c44842325f50032b1d124
csacenda <cysacenda@gmail.com>
2017-09-15 18:10:42 +0200

    working on planning algo

--- server/src/algo/planning-algorithm.ts
+++ server/src/algo/planning-algorithm.ts
@@ -7,24 +7,12 @@ export class PlanningAlgo {
   // TODO : Config file
   private dbAdress: string = 'mongodb://localhost:27017/planning-csa';
 
-  public CalculFullPlanning() {
-    this.OpenConnection();
-
-    // Récupérer resources
-    // Pour chaque resource, appeler CalculPlanningForResource
-
-    this.CloseConnection();
-  }
-
   public async CalculPlanningForResource(resourceTrigram: string, closeConnection?: boolean) {
     await this.OpenConnection();
 
     // Récupération paramètres du planning
     const planningParams = await PlanningParamsModel.findOne();
-    const currentDate = planningParams.currentDate;
-
-    // let taskDays: Map<string, number>;
-    // taskDays = new Map(taskArray.map((i) => [i.key, parseFloat(i.vacationDate)]));
+    let currentDate = planningParams.currentDate;
 
     // Récupérer toutes les taches d'une resource, non terminées (etc <> 0) triées par position
     const planningTasksCursor = await PlanningTaskModel.find({
@@ -40,14 +28,44 @@ export class PlanningAlgo {
           {resourceTrigram: 'ALL'}
         ]
       }).sort({val: 1});
+    // Récupération d'un tableau d'objets
+    const planningVacationsObj = await planningVacations.map(vacation => vacation.toObject());
+    let tmpVacationsArray: any;
+    tmpVacationsArray = planningVacationsObj;
+    const planningVacationMap = await new Map<string, number>(tmpVacationsArray.map((i) => [i.vacationDate.toJSON(), i.value]));
 
     // Tant que RAE tâche > 0, en fct dispo collab, alimente tableau de dates
     for (let doc = await planningTasksCursor.next(); doc != null; doc = await planningTasksCursor.next()) {
+      // Créer un tableau de dates avec structure [{key: XX, val: XX},{key: XX, val: XX}]
+      const datesValuesMap = new Array();
+      let etcToPlannify = doc.etc;
+      while (etcToPlannify > 0) {
+        // Pour la date en cours, récupérer dans planningVacations la charge
+        // Prendre plus petit entre 1-planningVacations ET etc
+        let availableAfterVacation: number = 1;
+        if (planningVacationMap.has(currentDate.toJSON())) {
+          availableAfterVacation = 1 - planningVacationMap.get(currentDate.toJSON());
+        }
+        const nbDaysToPlannify = Math.min(availableAfterVacation, etcToPlannify);
+        if (nbDaysToPlannify > 0) {
+          datesValuesMap.push({key: JSON.stringify(currentDate), val: nbDaysToPlannify});
+          etcToPlannify = etcToPlannify - nbDaysToPlannify;
+        }
 
-      console.log(doc.workload);
+        // Si il ne reste plus de charge planifiable sur la date
+        if (nbDaysToPlannify + availableAfterVacation >= 1) {
+          currentDate = this.getNextOpenDay(currentDate);
+        }
+
+        // Si dernière itération
+        if (etcToPlannify === 0) {
+          console.log(doc.resourceTrigram + ' - ' + doc.name);
+          console.log(datesValuesMap);
+          doc.daysMap = datesValuesMap;
+          doc.save();
+        }
+      }
     }
-
-
     /* if (typeof(closeConnection) === 'boolean' && closeConnection) {
       this.CloseConnection();
     } */
@@ -65,7 +83,26 @@ export class PlanningAlgo {
     });
   }
 
+  public CalculFullPlanning() {
+    this.OpenConnection();
+
+    // Récupérer resources
+    // Pour chaque resource, appeler CalculPlanningForResource
+
+    this.CloseConnection();
+  }
+
   private CloseConnection() {
     mongoose.connection.close();
+  }
+
+  private getNextOpenDay(date: Date): Date {
+    // Si vendredi, passe à lundi
+    let nbDaysToAdd = 1;
+    if (date.getDay() === 5) {
+      nbDaysToAdd = 3;
+    }
+    date.setDate(date.getDate() + nbDaysToAdd);
+    return date;
   }
 }
